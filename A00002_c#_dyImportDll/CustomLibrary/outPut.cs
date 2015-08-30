f3b0741b3c8b51f1b90bc3b61fd2004d71c6a6f1
LuCloudCode <shouhuni.xue@qq.com>
2015-08-30 19:15:29 +0800

    c#动态加载dll
    c#动态加载dll、实现特定路径下dll动态加载。。

---
+++ A00002_c#_dyImportDll/CustomLibrary/outPut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CustomLibrary
+{
+    public class outPut
+    {
+        public string ReturnString()
+        {
+            return "function call success...";
+        }
+    }
+}
---
+++ A00002_c#_dyImportDll/TestImportDll/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomLibrary;
+namespace TestImportDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            outPut op = new outPut();
+
+            Console.WriteLine(op.ReturnString());
+            Console.ReadKey();
+        }
+    }
+}
